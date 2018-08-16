7efdd264e0f1f581f41ddf191fa7b4fe20fad4cd
dzucconi <mail@damonzucconi.com>
2018-08-16 10:37:28 -0400

    Prototypes masking technique

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -6,8 +6,22 @@ const DOM = {
   app: document.getElementById('app'),
 };
 
-export default () => {
-  const { message } = parameters({ message: 'Hello' });
+const SRCS = [
+  'https://atlas-production.s3.amazonaws.com/10329/4976f520a78ad08a6c23d5855639b6842e8b496f01c7d1402d4120d148aa5af1.jpg',
+  'https://atlas-production.s3.amazonaws.com/10353/2c22bd812af91623008d92529b12010240b449d35009a1c02d24c20656da7f6c.jpg',
+];
 
-  DOM.app.innerHTML = message;
+export default () => {
+  const img = new Image;
+  img.src = SRCS[0];
+  img.classList.add('mask--a');
+  img.classList.add('hue--90');
+
+  const img2 = new Image;
+  img2.src = SRCS[1];
+  img2.classList.add('mask--b');
+  img2.classList.add('hue--360');
+
+  DOM.app.appendChild(img);
+  DOM.app.appendChild(img2);
 };
